998311111901f93c9a289a31dbd8398acc4e0ced
mohammadkhawandy <kmm826185@gmail.com>
2025-07-26 12:45:52 +0300

    update

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,11 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const authRoutes = require('./auth.routes');
-const userRoutes = require('./user.routes');
+const authRoutes = require("./auth.routes");
+const userRoutes = require("./user.routes");
+const orderRoutes = require("./order.routes");
 
-router.use('/auth', authRoutes);
-router.use('/users', userRoutes);
+router.use("/auth", authRoutes);
+router.use("/users", userRoutes);
+router.use("/orders", orderRoutes);
 
 module.exports = router;
