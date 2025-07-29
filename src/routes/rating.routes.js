1a63552ccf803d440f609307d006d1acd36d7c73
heveyhasan <heveyhasan97@gmail.com>
2025-07-30 02:31:37 +0300

    Add Average Rating Files

--- src/routes/rating.routes.js
+++ src/routes/rating.routes.js
@@ -3,12 +3,12 @@ const router = express.Router();
 const { addRating, getDriverRatings } = require('../controllers/ratingController');
 const { protect, authorize } = require('../middleware/authMiddleware');
 
-//  POST /ratings/api
-//  Create a new rating for a driver (Client only)
-router.post('/ratings/api', protect, authorize('Client'), addRating);
 
-//  GET /driverId/:driver/ratings/api
+//  Create a new rating for a driver (Client only)
+router.post('/', protect, authorize('Client'), addRating);
+
+ 
 //  Get all ratings for a specific driver (Public access)
-router.get('/driverId/:driver/ratings/api', getDriverRatings);
+router.get('/:driverId', getDriverRatings);
 
 module.exports = router;
